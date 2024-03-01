dcdb93bceffb6a51b8c1b10665676eb202a97ab2
SegunCodes <shegstix64@gmail.com>
2024-03-02 00:42:08 +0100

    logical operators

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿// console commands
-using System.Reflection.Metadata;
-
 Console.WriteLine("Hi!");
 Console.WriteLine("\t Jon is outside");
 Console.Write("Okay!");
@@ -39,7 +37,102 @@ Console.WriteLine(a.GetType());
 // int to string
 int c = 419;
 String d = Convert.ToString(c);
-
 Console.WriteLine(d);
 
-Console.ReadLine();
+// arithmetic operators : +, -, / , %, *
+double school = 5;
+//school = school + 2;
+//school +=2;
+//school++; //increment by 1
+
+//school = school - 2;
+//school -= 2;
+
+//school = school / 2;
+//school /= 2;
+
+// school = school % 2;
+//school = school * 2;
+Console.WriteLine(school);
+
+String fullName = "john doe";
+String email = "john@doe.com";
+
+Console.WriteLine("Enter your name");
+String yourName = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Enter your email");
+String yourEmail = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Enter a password");
+String password = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Confirm password");
+String confirmPassword = Convert.ToString(Console.ReadLine());
+if (password.Length < 6 || password.Length > 14)
+{
+    Console.WriteLine("Password length must not be less than 6 or exceed 14 characters");
+}
+else if (!password.Any(char.IsUpper))
+{
+    Console.WriteLine("Password must contain a capital letter");
+}
+else if (password != confirmPassword)
+{
+    Console.WriteLine("Passwords do not match");
+}
+else if(yourName != fullName && yourEmail != email)
+{
+    Console.WriteLine("Invalid username or email");
+}
+else
+{
+    Console.WriteLine("Successful");
+}
+// int16 -> 99999
+// int32 -> 999,999,999
+// int64 -> 999,999,999,999, 999, 999
+
+
+//pythagoras theorem, p^2 = m^2 + n^2
+Console.WriteLine("Enter a value for a");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter a value for b");
+double n = Convert.ToDouble(Console.ReadLine());
+double p = Math.Sqrt((m * m) + (n * n));
+Console.WriteLine("The hypotenuse is " + p);
+
+// = used for assigning a value to a variable or constant
+// == used for checking equality
+
+// Conditional statements
+if (p == 5)
+{
+    Console.WriteLine("Congratulations, you are HIM");
+}
+else if (p == 8)
+{
+    Console.WriteLine("Congratulations, you are a lucky winner");
+}
+else
+{
+    Console.WriteLine("Try again to get a value of 8 or 5");
+}
+
+//logical operators
+// ==
+// > 
+// < 
+// >= i.e greater than or equal to
+// <= i.e less than or equal to
+// && i.e AND
+// || i.e OR
+// ! i.e NOT
+
+// accepting inputs
+//Console.WriteLine("What is your name?");
+//String name = Console.ReadLine();
+//Console.WriteLine("Hello " + name);
+//Console.WriteLine("How old are you?");
+//int age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Your name is "+ name + " and you are " + age + " years old");
+
+
+Console.ReadKey();
