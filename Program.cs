d1e0632de07781d4385386737dbf9d8feab892a3
SegunCodes <shegstix64@gmail.com>
2024-03-06 00:22:27 +0100

    for and while loops

--- Program.cs
+++ Program.cs
@@ -1,29 +1,36 @@
 ﻿//switch statement
-Console.WriteLine("Input a car name");
-String name = Console.ReadLine();
+using System;
 
-switch (name)
+// while loop : The while loop is used to repeat a section of code an "unknown" number of times until a specific condition is met
+// String name = "";
+// while (name == "")
+// {
+// Console.WriteLine("Input a car name");
+// name = Console.ReadLine();
+// }
+
+// For loop : "For" Loop is used to repeat a specific block of code a "known" number of times
+// list all natural numbers using a for loop
+// for (int i = 0; i < 10; i+=2)
+// {
+// Console.WriteLine(i);
+// }
+
+// nested loops is a loop inside a loop
+Console.WriteLine("How many rows?");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("How many columns?");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter an alphabet");
+String alphabet = Console.ReadLine();
+for (int i = 0; i < rows; i++)
 {
-    case "Honda":
-        Console.WriteLine(name + " is a car");
-        break;
-    case "Chevrolet":
-        Console.WriteLine(name + " is a car");
-        break;
-    case "Lexus":
-        Console.WriteLine(name + " is a car");
-        break;
-    case "Rolls Royce":
-        Console.WriteLine(name + " is a car");
-        break;
-    case "Benz":
-        Console.WriteLine(name + " is a car");
-        break;
-    default:
-        Console.WriteLine(name + " is not  a car");
-        break;
-
-
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(alphabet); 
+    }
+    Console.WriteLine();
 }
+
 
 Console.ReadKey();
